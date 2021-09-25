2a77e00a326e1e939c28a73712fc0e5e89e01260
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2021-09-25 19:13:23 +0800

    代码中增加布局约束

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -6,8 +6,31 @@
 //
 
 import UIKit
+import MapKit
+
 
 class MapViewControlloer:UIViewController{
+    var mapView:MKMapView!
+
+    override func loadView() {
+        mapView = MKMapView()
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = .systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.topAnchor)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
