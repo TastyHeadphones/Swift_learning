840bd97b49bd80edf58ba9ee3a66e415c9478d25
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2021-09-25 11:19:39 +0800

    第四章节内容

---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Magic Keegan on 9/25/21.
+//
+
+import UIKit
+
+class MapViewControlloer:UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Map is loaded")
+    }
+}
