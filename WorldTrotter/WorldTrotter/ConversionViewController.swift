b86aa969cff84bc1fbfe8b502a56b11936776998
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2021-09-25 10:55:15 +0800

    每次启动更改颜色

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -7,19 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class ConversionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = [UIColor.red.cgColor,
-                               UIColor.yellow.cgColor,
-                               UIColor.green.cgColor,
-                               UIColor.blue.cgColor]
+        print("Conversion view is called")
+//        let gradientLayer = CAGradientLayer()
+//        gradientLayer.colors = [UIColor.red.cgColor,
+//                               UIColor.yellow.cgColor,
+//                               UIColor.green.cgColor,
+//                               UIColor.blue.cgColor]
 //        gradientLayer.transform = CATransform3DMakeRotation(CGFloat.pi / 2, 0, 0, 1)
-        gradientLayer.frame.size = view.frame.size
-        view.layer.insertSublayer(gradientLayer,at: 0)
+//        gradientLayer.frame.size = view.frame.size
+//        view.layer.insertSublayer(gradientLayer,at: 0)
 //        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
 //        let firstView = UIView(frame: firstFrame)
 //        firstView.backgroundColor = .blue
@@ -36,7 +37,11 @@ class ViewController: UIViewController {
 //        view.addSubview(secondView)
         // Do any additional setup after loading the view.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        view.backgroundColor = UIColor(hue: CGFloat.random(in: 0...1), saturation: 1.0, brightness: 1.0, alpha: 1.0)
+        
+    }
 
 }
 
---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Magic Keegan on 9/25/21.
+//
+
+import UIKit
+
+class MapViewController:UIViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Map view is called")
+
+    }
+}
