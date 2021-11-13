96f75c50e20ac240643420bb704b965f529140f5
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2021-11-13 15:20:24 +0800

    Chapter 17

---
+++ Mandala/Mandala/MoodEntry.swift
@@ -0,0 +1,13 @@
+//
+//  MoodEntry.swift
+//  Mandala
+//
+//  Created by Magic Keegan on 10/31/21.
+//
+
+import Foundation
+
+struct MoodEntry{
+    var mood: Mood
+    var timestamp: Date
+}
---
+++ Mandala/Mandala/MoodsConfigurable.swift
@@ -0,0 +1,12 @@
+//
+//  MoodsConfigurable.swift
+//  Mandala
+//
+//  Created by Magic Keegan on 11/10/21.
+//
+
+import Foundation
+
+protocol MoodsConfigurable {
+    func add(_ moodEntry: MoodEntry)
+}
